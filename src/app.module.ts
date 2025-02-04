f402ef6245e7d883fe5ce4a95d3835b0a9490025
RyanCFX <rcastrofelix06@gmail.com>
2025-02-04 06:00:23 -0600

    feat: agrega configuracion de base de datos y agrega modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,54 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { Transaction } from './transaction/transaction.entity';
+import { ValidationCode, ValidationCodeType } from './auth/auth.entity';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { Status } from './status/status.entity';
+import { StatusModule } from './status/status.module';
+import { MedStatusTask, Task } from './task/task.entity';
+import { TaskModule } from './task/task.module';
+import { GroupModule } from './group/group.module';
+import { Group, MedGroupUser } from './group/group.entity';
+import { TodoGateway } from './task/task.socket';
+import { ConfigModule } from '@nestjs/config';
+
+const entities = [
+  User,
+  Transaction,
+  ValidationCode,
+  ValidationCodeType,
+  Status,
+  Task,
+  Group,
+  MedGroupUser,
+  MedStatusTask,
+];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Permite acceder a process.env desde cualquier módulo
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities,
+      synchronize: false,
+      logging: true,
+      ssl: true,
+    }),
+    UserModule,
+    AuthModule,
+    StatusModule,
+    TaskModule,
+    GroupModule,
+  ],
+  controllers: [],
+  providers: [TodoGateway],
 })
 export class AppModule {}
