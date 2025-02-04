e527a6825b93d8198e402d9a51009f008f3fee02
RyanCFX <rcastrofelix06@gmail.com>
2025-02-04 06:00:40 -0600

    refactor: agrega configuracion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as requestIp from 'request-ip';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // LEER COOKIES
+  app.use(cookieParser());
+  app.use(requestIp.mw());
+
+  // LIMITE DE TAMAÑO DEL BODY
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+  app.enableCors({
+    origin: [''],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization', // Agrega Content-Type y otros headers necesarios
+    credentials: true, // Si necesitas cookies o autenticación
+  });
+
+  await app.listen(process.env.PORT || 9876);
+  // await app.listen(3005);
 }
 bootstrap();
